93c8a78a97a3b935958ad4fcbeb8412db38b1cd2
Landon Conover <landon.conover@gmail.com>
2016-01-19 16:42:47 -0700

    removing stuff

--- aboutme.js
+++ aboutme.js
@@ -1,7 +1,5 @@
+
+//Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 exports.student = {
-    name:'landonconover',
-    age: 29,
-    gender: 'male',
-    bio: 'This is something about me. I like to go for long walks on the beach and do lots and lots of stuff. I also am very very very cool. I am writing lots of test and stuff.',
-    something: 'yep'
+
 }
