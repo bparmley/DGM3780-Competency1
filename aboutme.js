ff374f3387f809175ccae85a52b83eaec52f4f5f
Landon Conover <landon.conover@gmail.com>
2016-01-19 16:19:24 -0700

    Added prop

--- aboutme.js
+++ aboutme.js
@@ -2,5 +2,6 @@ exports.student = {
     name:'landonconover',
     age: 29,
     gender: 'male',
-    bio: 'This is something about me. I like to go for long walks on the beach and do lots and lots of stuff. I also am very very very cool. I am writing lots of test and stuff.'
+    bio: 'This is something about me. I like to go for long walks on the beach and do lots and lots of stuff. I also am very very very cool. I am writing lots of test and stuff.',
+    something: 'yep'
 }
