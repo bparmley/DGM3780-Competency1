5bda4623c070854a9be40348da723df8a1c51796
Thor Anderson <ta.anderson@gmail.com>
2016-01-20 09:10:51 -0700

    Did the assignment steps and made the ReadMe file a little more verbose.

--- aboutme.js
+++ aboutme.js
@@ -1,5 +1,8 @@
 
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 exports.student = {
-
+    name: 'Thor Anderson',
+    gender: 'Male',
+    age: 49,
+    bio: 'I am the professor who is teaching this course. I have worked in Silicon Valley and I have been teaching for 10 years. I teach various web development skills.'
 }
