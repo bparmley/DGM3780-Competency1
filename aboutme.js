b425c1f5f8f2e5680ba66d22ac2923712c716448
Brandon Parmley <orem.bparmley@gmail.com>
2016-01-25 10:25:04 -0700

    Add Javascript object name Brandon Parmley

--- aboutme.js
+++ aboutme.js
@@ -1,4 +1,3 @@
-
 //Construct an object with the following properties: name(String), age(number), gender(String), bio(String, longer than 140 chars)
 exports.student = {
     name: 'Thor Anderson',
@@ -6,4 +5,10 @@ exports.student = {
     age: 49,
     bio: 'I am the professor who is teaching this course. I have worked in Silicon Valley and I have been teaching for 10 years. I teach various web development skills.',
     interests: ['computers', 'teaching', 'crossfit']
+}, {
+    name: 'Brandon Parmley',
+    gender: 'Male',
+    age: 31,
+    interests: ['Star Wars', 'Coding', 'X Files'],
+    bio: 'I am a web developer at Utah Valley University. I am currently working on the myUVU project in charge of rebuilding the university\'s portal system from the ground up.'
 }
